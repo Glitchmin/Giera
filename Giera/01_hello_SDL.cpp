18d056b3008160cd77d5baf38dfcf9b8b7ee126b
Michał Zarębski <michal.zarebski@onet.pl>
2022-02-17 18:29:36 +0000

    Added SDL_image and SDL_ttf

--- Giera/01_hello_SDL.cpp
+++ Giera/01_hello_SDL.cpp
@@ -17,7 +17,7 @@ int main( int argc, char* args[] )
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
 
-	//Initialize SDL
+	//Initialize SDL 
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
