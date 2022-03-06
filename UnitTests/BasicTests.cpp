dd7a6254389557df7ab6f48925f7616361e766fc
Glitchmin <adam.p.kulczycki@gmail.com>
2022-03-06 20:39:30 +0100

    tests code cleanup

--- UnitTests/BasicTests.cpp
+++ UnitTests/BasicTests.cpp
@@ -17,6 +17,7 @@ namespace UtilityTests
 	TEST_CLASS(TimerTests)
 	{
 	public:
+		//those tests might not be working is launched on a VERY slow (or occupied with something else) device. Just rerun them before drawing conclusions
 		TEST_METHOD(GeneralTimerTestTempo1_0)
 		{
 			GeneralTimer generalTimer;
@@ -78,55 +79,55 @@ namespace UtilityTests
 		TEST_METHOD(SubTimerTest)
 		{
 			std::shared_ptr <GeneralTimer> generalTimer(new GeneralTimer);
-			Assert::IsTrue((*generalTimer).getTime() < 5);
+			Assert::IsTrue(generalTimer->getTime() < 5);
 			Uint32 time = SDL_GetTicks();
 			std::shared_ptr<SubTimer> subTimer (new SubTimer(generalTimer));
 			while (SDL_GetTicks() < time + 200)
 			{
-				(*generalTimer).updateTime();
-				(*subTimer).updateTime();
+				generalTimer->updateTime();
+				subTimer->updateTime();
 			}
-			Assert::IsTrue((*subTimer).getTime() < 210);
-			Assert::IsTrue((*subTimer).getTime() > 190);
+			Assert::IsTrue(subTimer->getTime() < 210);
+			Assert::IsTrue(subTimer->getTime() > 190);
 		}
 		TEST_METHOD(SubTimerTestDiffTempos)
 		{
 			std::shared_ptr <GeneralTimer> generalTimer(new GeneralTimer);
-			Assert::IsTrue((*generalTimer).getTime() < 5);
+			Assert::IsTrue(generalTimer->getTime() < 5);
 			Uint32 time = SDL_GetTicks();
 			std::shared_ptr<SubTimer> subTimer(new SubTimer(generalTimer));
-			(*generalTimer).setTempo(2.0);
-			(*subTimer).setTempo(0.5);
+			generalTimer->setTempo(2.0);
+			subTimer->setTempo(0.5);
 			while (SDL_GetTicks() < time + 200)
 			{
-				(*generalTimer).updateTime();
-				(*subTimer).updateTime();
+				generalTimer->updateTime();
+				subTimer->updateTime();
 			}
-			Assert::IsTrue((*subTimer).getTime() < 210);
-			Assert::IsTrue((*subTimer).getTime() > 190);
+			Assert::IsTrue(subTimer->getTime() < 210);
+			Assert::IsTrue(subTimer->getTime() > 190);
 		}
 		TEST_METHOD(SubTimerPauseTest)
 		{
 			std::shared_ptr <GeneralTimer> generalTimer(new GeneralTimer);
-			Assert::IsTrue((*generalTimer).getTime() < 5);
+			Assert::IsTrue(generalTimer->getTime() < 5);
 			Uint32 time = SDL_GetTicks();
 			std::shared_ptr<SubTimer> subTimer(new SubTimer(generalTimer));
-			(*generalTimer).setTempo(2.0);
-			(*subTimer).pause();
+			generalTimer->setTempo(2.0);
+			subTimer->pause();
 			while (SDL_GetTicks() < time + 100)
 			{
-				(*generalTimer).updateTime();
-				(*subTimer).updateTime();
+				generalTimer->updateTime();
+				subTimer->updateTime();
 			}
 			time = SDL_GetTicks();
-			(*subTimer).unpause();
+			subTimer->unpause();
 			while (SDL_GetTicks() < time + 100)
 			{
-				(*generalTimer).updateTime();
-				(*subTimer).updateTime();
+				generalTimer->updateTime();
+				subTimer->updateTime();
 			}
-			Assert::IsTrue((*subTimer).getTime() < 210);
-			Assert::IsTrue((*subTimer).getTime() > 190);
+			Assert::IsTrue(subTimer->getTime() < 210);
+			Assert::IsTrue(subTimer->getTime() > 190);
 		}
 	};
 }
