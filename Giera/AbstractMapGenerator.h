b2d71d1147ab9c9348545ae7fd737374084048d9
Glitchmin <adam.p.kulczycki@gmail.com>
2022-03-13 16:27:36 +0100

    Grasslands Generator Added

--- Giera/AbstractMapGenerator.h
+++ Giera/AbstractMapGenerator.h
@@ -1,5 +1,12 @@
 #pragma once
+#include "ValuesRange.h"
+#include <memory.h>
+#include "Map.h"
 class AbstractMapGenerator
 {
+protected:
+	ValuesRange walkableTilesRatio;
+public:
+	virtual void generateMap(std::shared_ptr<Map> map) = 0;
 };
 
---
+++ Giera/GrasslandsGenerator.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "AbstractMapGenerator.h";
+class GrasslandsGenerator: public AbstractMapGenerator
+{
+
+};
+
