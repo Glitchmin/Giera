f6ae0bbbf0c090dd6ab667662233f100264ee49a
Adam <adam.p.kulczycki@gmail.com>
2022-04-25 13:46:22 +0200

    Damage and Lifesteal added

---
+++ Giera/Lifesteal.cpp
@@ -0,0 +1,19 @@
+#include "Lifesteal.h"
+
+Lifesteal::Lifesteal(double value, DamageTypes damageType, shared_ptr<AbstractNPC> damageDealer, double percentStolen, double probability):
+    Damage(value, damageType, damageDealer)
+{
+    this->percentStolen = percentStolen;
+    this->probability = probability;
+}
+
+double Lifesteal::getProbability() const
+{
+    return probability;
+}
+
+double Lifesteal::getPercentStolen() const
+{
+    return percentStolen;
+}
+
---
+++ Giera/Lifesteal.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Damage.h"
+class Lifesteal :
+    public Damage
+{
+private:
+    double probability;
+    double percentStolen;
+public:
+    Lifesteal(double value, DamageTypes damageType, shared_ptr<AbstractNPC> damageDealer,
+        double percentStolen, double probability = 1.0);
+
+    double getProbability() const;
+
+    double getPercentStolen() const;
+
+};
+
