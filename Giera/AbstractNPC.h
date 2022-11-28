987156cbd5917e21b4aaee7da3626f13ada5d45e
Adam <adam.p.kulczycki@gmail.com>
2022-11-28 17:23:19 +0100

    Added AbstractNPC prototype

--- Giera/AbstractNPC.h
+++ Giera/AbstractNPC.h
@@ -1,6 +1,17 @@
 #pragma once
+#include "Position.h"
+#include "NPCTypes.h"
+using npc_hp_t = typename unsigned int;
 class AbstractNPC
 {
-	//mock class to make pointers to
+protected:
+	npc_hp_t hp;
+	Position position;
+	vector <double> resitances;
+	double armor;
+	Inventory intentory;
+	vector <SpawningDetails> drops;
+	NPCTypes npcType;
+	Position target;
 };
 
