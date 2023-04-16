55624d413fbee7c7512d691d2d2afa88c920c67f
Glitchmin <adam.p.kulczycki@gmail.com>
2023-04-16 16:10:35 +0200

    Implemented ProjectilesEngine prototpye

--- Giera/ProjectilesEngine.cpp
+++ Giera/ProjectilesEngine.cpp
@@ -1,8 +1,55 @@
 #include "ProjectilesEngine.h"
 
+void ProjectilesEngine::threadFunction(int id)
+{
+	while (1) {
+		std::unique_lock <mutex> lck(projCalculationMutex);
+		while (!readyToCalculate.load()) {
+			projShouldBeCalculated.wait(lck);
+		}
+		if (threadsShouldJoin.load()) {
+			return;
+		}
+
+		Logger::logInfo(id);
+
+		int threadsFinished = ++threadsFinishedCounter;
+		if (threadsFinished == threadsNumber) {
+			allThreadsFinishedCv.notify_one();
+		}
+
+	}
+}
+
 ProjectilesEngine::ProjectilesEngine(weak_ptr<Board> board)
 {
 	this->board = board;
-	threads.resize(4); //TO DO make it depentant on some constant
+	readyToCalculate.store(0);
+	threadsFinishedCounter.store(0);
 
+	for (int i = 0; i < threadsNumber; i++) {
+		threads.push_back(thread(&ProjectilesEngine::threadFunction,this, i));
+	}
+	projIndexesToRemovePerThread.resize(threadsNumber);
+
+}
+
+ProjectilesEngine::~ProjectilesEngine()
+{
+	threadsShouldJoin.store(1);
+	for (auto& th : threads) {
+		th.join();
+	}
+}
+
+void ProjectilesEngine::calculateProjectiles(Time timeDiff, vector<AbstractProjectile>& projectiles, vector<AbstractProjectile>& projectilesToAdd)
+{
+	auto boardSp = board.lock();
+	threadsFinishedCounter.store(0);
+	readyToCalculate.store(1);
+	projShouldBeCalculated.notify_all();
+
+	std::unique_lock <mutex> lck(allThreadsFinishedMtx);
+	allThreadsFinishedCv.wait(lck);
+	
 }
--- Giera/ProjectilesEngine.h
+++ Giera/ProjectilesEngine.h
@@ -1,16 +1,36 @@
 #pragma once
 #include "AbstractProjectile.h"
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
 #include <thread>
 
+using std::atomic;
+using std::thread;
+using std::mutex;
+using std::condition_variable;
 
 class ProjectilesEngine
 {
 protected:
 	weak_ptr <Board> board;
-	vector <std::thread> threads;
+	vector <thread> threads;
+	atomic <bool> readyToCalculate;
+	atomic <bool> threadsShouldJoin;
 	vector <vector <int> > projIndexesToRemovePerThread;
+	mutex projCalculationMutex;
+	condition_variable projShouldBeCalculated;
+	atomic <int> threadsFinishedCounter;
+	mutex allThreadsFinishedMtx;
+	condition_variable allThreadsFinishedCv;
+	int threadsNumber = 4; //TO DO make it depentant on some constant
+
+
+	void threadFunction(int id);
 public:
 	ProjectilesEngine(weak_ptr<Board> board);
+	ProjectilesEngine(ProjectilesEngine& pe) = delete;
+	~ProjectilesEngine();
 	void calculateProjectiles(Time timeDiff, vector <AbstractProjectile>& projectiles, vector <AbstractProjectile>& projectilesToAdd);
 };
 
